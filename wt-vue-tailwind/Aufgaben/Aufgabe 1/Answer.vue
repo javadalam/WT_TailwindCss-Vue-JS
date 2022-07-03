769fd42128c525e731cb337d84edab1325be10be
javadalam <javad.alamdar@smail.th-koeln.de>
2022-07-03 17:22:07 +0200

    add lösung für Aufgabe 1

---
+++ wt-vue-tailwind/Aufgaben/Aufgabe 1/Answer.vue
@@ -0,0 +1,34 @@
+<script setup>
+
+</script>
+
+<template>
+  
+   <div class="relative flex min-h-screen">
+
+    <!-- Aufgabe 1 -->
+    
+      <div class="bg-cyan-400 w-64 space-y-6">
+          <a href="" class=""> <span > Home </span> </a>
+         
+        <nav>
+          <a href="/" class="flex"> <span> Dashboard </span> </a>
+           
+          <a href="/" class="flex"> <span> Team </span> </a>
+           
+          <a href="/" class="flex"> <span> Project </span> </a>
+          
+          <a href="/" class="flex"> <span> Calender </span> </a>
+        </nav>
+         
+      </div>
+         
+    
+      <div class="flex-1 bg-green-300">Header
+
+        <div class="pt-8"> Content </div> 
+      </div>
+  </div>
+ 
+</template>
+
